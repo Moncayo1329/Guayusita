1c2fba4889b2a40775b2d756e928e709cdfcb2c5
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-07-22 18:51:21 +0100

    finish first page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Inicio from './Menu';
+import Nosotros from './Nosotros';
 
 function App() {
   return (
     <div className="App">
       <Inicio />
+      <Nosotros />
     </div>
   );
 }
---
+++ src/Nosotros.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nosotros(){
+
+    <>
+<h1>Que es Guayusita?</h1>
+<p>bla bla bla bla bla bla bla bla </p>
+    </>
+
+}
+
+
+
+
+export default Nosotros;
