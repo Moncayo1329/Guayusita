b41ba20645a3d6354a9c45cc02808e3eda3b2ead
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-07-23 18:42:10 +0100

    add

--- src/Nosotros.js
+++ src/Nosotros.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function About(){
 
     return(
+        <div>
     <div className="Container">
 <h1>Que es Guayusita?</h1>
 <p>En Guayusita, creemos en el poder de la comunidad y 
@@ -13,6 +14,25 @@ function About(){
      a personas <br></br> con grandes ideas y aquellos que desean apoyarlas.
      <br></br>  Nuestra misión es ayudarte a transformar tus sueños en realidad y <br></br> brindarte el respaldo que necesitas cuando más lo necesitas.</p>
     </div>
+
+    <div className="about">
+        <p>1. Creación de Proyectos
+Registro: Los creadores se registran en nuestra plataforma y crean un perfil personal. Solo te llevará unos minutos personalizar tu perfil y comenzar a compartir lo que haces.
+Inicio de Proyecto: Una vez registrado, puedes iniciar tu propio proyecto. Describe tu idea, establece una meta de financiación y el tiempo durante el cual deseas recibir donaciones.
+2. Exploración y Donaciones
+Explorar Proyectos: Los usuarios pueden explorar los diferentes proyectos disponibles en la plataforma. Puedes navegar por categorías y encontrar proyectos que te interesen.
+Realizar Donaciones: Si encuentras un proyecto que te inspire, puedes hacer una donación. Las donaciones pueden ser de cualquier monto y, en algunos casos, los creadores ofrecen recompensas especiales para los donantes.
+3. Seguimiento de Proyectos
+Actualizaciones de Proyectos: Los creadores pueden actualizar a sus donantes sobre el progreso del proyecto. Esto incluye hitos alcanzados, novedades y cualquier otro detalle relevante.
+Comentarios y Feedback: Los donantes pueden dejar comentarios y sugerencias para los proyectos que apoyan, creando una comunidad interactiva y de apoyo.
+4. Recompensas y Finalización
+Entrega de Recompensas: Si el proyecto incluye recompensas, los creadores las entregarán una vez alcanzada la meta de financiación o completado el proyecto.
+Finalización del Proyecto: Al concluir el plazo de financiación, los fondos recaudados se transferirán al creador del proyecto, menos una pequeña comisión que ayuda a mantener la plataforma.
+</p>
+    </div>
+
+    </div>
+   
 );
 }
 
