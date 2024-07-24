e16e559171c9ee0721df465650cd5422c38bbc9c
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-07-24 02:41:23 +0100

    adding styles

--- src/Menu.js
+++ src/Menu.js
@@ -5,15 +5,24 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function Inicio() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
+    <div className="container-fluid">
       <a className="navbar-brand" href="#">Guayusita</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav custom-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Explorar</a>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Explorar</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Ingresar</a>
@@ -23,7 +32,8 @@ function Inicio() {
           </li>
         </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
   );
 }
 
--- src/Nosotros.js
+++ src/Nosotros.js
@@ -14,22 +14,25 @@ function About(){
      a personas <br></br> con grandes ideas y aquellos que desean apoyarlas.
      <br></br>  Nuestra misión es ayudarte a transformar tus sueños en realidad y <br></br> brindarte el respaldo que necesitas cuando más lo necesitas.</p>
     </div>
-
+<div className="list">
     <div className="about1">
-        <p>1. Creación de Proyectos
- Solo te llevará unos minutos personalizar tu perfil y comenzar a compartir lo que haces.
- Una vez registrado, puedes iniciar tu propio proyecto.<br></br> Describe tu idea, establece una meta de financiación y el tiempo durante el cual deseas recibir donaciones.</p> </div>
-<div className="about2"> 2. Exploración y Donaciones
-Explorar Proyectos: Los usuarios pueden explorar los diferentes proyectos disponibles en la plataforma. Puedes navegar por categorías y encontrar proyectos que te interesen.
-Realizar Donaciones: Si encuentras un proyecto que te inspire, puedes hacer una donación. Las donaciones pueden ser de cualquier monto y, en algunos casos, los creadores ofrecen recompensas especiales para los donantes.</div>
-<div className="about3">3. Seguimiento de Proyectos
-Actualizaciones de Proyectos: Los creadores pueden actualizar a sus donantes sobre el progreso del proyecto. Esto incluye hitos alcanzados, novedades y cualquier otro detalle relevante.
-Comentarios y Feedback: Los donantes pueden dejar comentarios y sugerencias para los proyectos que apoyan, creando una comunidad interactiva y de apoyo.</div>
-<div className="about4">4. Recompensas y Finalización
-Entrega de Recompensas: Si el proyecto incluye recompensas, los creadores las entregarán una vez alcanzada la meta de financiación o completado el proyecto.
-Finalización del Proyecto: Al concluir el plazo de financiación, los fondos recaudados se transferirán al creador del proyecto, menos una pequeña comisión que ayuda a mantener la plataforma.
+        <h3> Creación de Proyectos</h3> 
+        <p> Solo te llevará unos minutos personalizar tu perfil y comenzar a compartir lo que haces.<br></br> Describe tu idea, establece una meta de financiación y el tiempo durante el cual deseas recibir donaciones.</p> </div>
+<div className="about2">
+    <h3>Explorar Proyectos</h3>.
+    Los usuarios pueden explorar los diferentes proyectos disponibles en la plataforma. Puedes navegar por categorías y encontrar proyectos que te interesen.
+  Si encuentras un proyecto que te inspire, puedes hacer una donación.</div>
+<div className="about3">
+    <h3>Seguimiento de Proyectos</h3>
+ Los creadores pueden actualizar a sus donantes sobre el progreso del proyecto. Esto incluye hitos alcanzados, novedades y cualquier otro detalle relevante.
+Los donantes pueden dejar comentarios y sugerencias para los proyectos que apoyan.</div>
+<div className="about4">
+    <h3>Recompensas y Finalización</h3> 
+ <p>Al concluir el plazo de financiación, los fondos recaudados se transferirán al creador del proyecto.</p>
 
     </div>
+
+    </div> 
 
     </div>
    
